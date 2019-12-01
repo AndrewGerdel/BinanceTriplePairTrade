a01484601d1a803ffb398e3d8ecc6045e3cfc790
Andrew Gerdel <AndrewGerdel@yahoo.com>
2019-12-01 08:42:49 -0600

    Not much action in 3 days

--- start.js
+++ start.js
@@ -1,3 +1,17 @@
+const argv = require('yargs').argv;
+var cryptoCurrency =  argv.crypto;
+if(!cryptoCurrency){
+    console.log('Missing crypto parameter');
+    return;
+}
+var maxSell =  argv.maxSell;
+if(!maxSell){
+    console.log('Missing maxSell parameter');
+    return;
+}
+console.log('Monitoring fo arb situations on', cryptoCurrency);
+
+
 const Binance = require('binance-api-node').default;  
 var config = require('./config.json');
 var binanceClient = Binance({
@@ -5,33 +19,35 @@ var binanceClient = Binance({
     apiSecret: config.apiSecret
 });
 
-//pair is XRPBTC, BTCUSD, etc.
 var GetBinancePrices = (async() => {
     var prices = await binanceClient.allBookTickers();
     return prices;    
-    // return {Bid: parseFloat(prices.pair.bidPrice).toFixed(8), BidVolume: prices.pair.bidQty, Ask: parseFloat(prices.pair.askPrice).toFixed(8), AskVolume: prices.pair.askQty};
 });
 
 var CompareAllThreePrices = (async () => {
     var allPrices = await GetBinancePrices();
-    var xrpSellQty = 1000;
-    var xrpSellTotalUSD = xrpSellQty * parseFloat(allPrices.XRPUSDT.bidPrice);
-    var btcBuyTotal = xrpSellTotalUSD / parseFloat(allPrices.BTCUSDT.askPrice);
-    var btcToXrpSellTotal = btcBuyTotal / parseFloat(allPrices.XRPBTC.bidPrice);
+    var sellQty = parseFloat(allPrices[cryptoCurrency + 'USDT'].bidQty);
+    //If the top buyer is offering more than our maxSell, only sell the maxSell
+    if(sellQty > parseFloat(maxSell)){
+        sellQty = maxSell;
+    }
+    var sellTotalUSD = sellQty * parseFloat(allPrices[cryptoCurrency + 'USDT'].bidPrice);
+    var btcBuyTotal = sellTotalUSD / parseFloat(allPrices.BTCUSDT.askPrice);
+    var btcToXrpSellTotal = btcBuyTotal / parseFloat(allPrices[cryptoCurrency + 'BTC'].bidPrice);
     
-    var xrpProfit = btcToXrpSellTotal - xrpSellQty;
+    var xrpProfit = btcToXrpSellTotal - sellQty;
     var usdProfit = xrpProfit * parseFloat(allPrices.XRPUSDT.bidPrice);
     
-    var totalCommision = (xrpSellTotalUSD * 0.00075) * 3;
+    var totalCommision = (sellTotalUSD * 0.00075) * 3;
 
     var totalProfit = (usdProfit - totalCommision);
 
     if(totalProfit > 0){
         console.log("------ Profitable trade setup as of ", new Date(), "-------");
-        console.log("You could currently sell 1,000 XRP for $", xrpSellTotalUSD);
-        console.log("You could currently buy $", xrpSellTotalUSD, "which is worth (b)", btcBuyTotal);
-        console.log("You could currently sell ", btcBuyTotal, "worth of Bitcoin for", btcToXrpSellTotal, "worth of XRP");
-        console.log("Total profit in XRP: ", xrpProfit, ".  Total profit in USD: $", usdProfit);
+        console.log("You could currently sell ", sellQty, cryptoCurrency,"for $", sellTotalUSD);
+        console.log("You could currently buy $", sellTotalUSD, "worth of Bitcoin, which is worth (b)", btcBuyTotal);
+        console.log("You could currently sell ", btcBuyTotal, "worth of Bitcoin for", btcToXrpSellTotal, "worth of ", cryptoCurrency);
+        console.log("Total profit in ", cryptoCurrency, ": ", xrpProfit, ".  Total profit in USD: $", usdProfit);
         console.log("Total commission in USD would be $", totalCommision);
         console.log("Total profit/loss: $", totalProfit);
     }
